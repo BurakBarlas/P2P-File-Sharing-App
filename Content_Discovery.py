056ee10447ca740e308511ca2a415b6eb1eaceec
Burak Barlas <fpburak@gmail.com>
2021-05-11 17:58:24 +0300

    Content_Discovery

--- Content_Discovery.py
+++ Content_Discovery.py
@@ -1,5 +1,7 @@
 import socket
+import json
 
+# 2.2.0-A
 serverPort = 5001
 
 # Create a UDP socket
@@ -9,8 +11,28 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
 
+
+contentDictionary = {
+    "chunks": []
+}
+
+with open('contentDictionary.txt', 'w') as outfile:
+    json.dump(contentDictionary, outfile)
+
+
 while 1:
-    message, clientAddress = serverSocket.recvfrom(2048)
+    message, clientAddress = serverSocket.recvfrom()
     print('received {} bytes from {}'.format(len(message), clientAddress))
+
+    # 2.2.0-B
+    contentDictionary = json.load(message)
+    contentDictionary["chunks"].push(clientAddress)
+
+    f = open("contentDictionary.txt", "wb+")
+    f.write(str(contentDictionary))
+    f.close()
+
+    print('"{}" : {}'.format(clientAddress, message))
+
     modifiedMessage = message.upper()
     serverSocket.sendto(modifiedMessage, clientAddress)
